039a4949b1adfea53ac701563617ba1798a854b5
Ajit Kumar <2003ajit@gmail.com>
2022-09-20 10:23:58 +0530

    jgj

--- config.php
+++ config.php
@@ -8,7 +8,6 @@ $dbname = "AWT";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    echo"khjkjh";
     die("Connection failed: " . $conn->connect_error);
     
   }
